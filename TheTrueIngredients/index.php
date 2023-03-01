04687a4cc9475d86a97481f4272707b65a9948ae
Ky000 <banana11122@wp.pl>
2023-03-01 19:32:03 +0100

    Add files via upload

---
+++ TheTrueIngredients/index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="Just a normal day on Underwearzip city.">
+	<link rel="icon" type="icon/image" href="icon/icon.png">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>The True Ingredients by RenderPi</title>
+</head>
+<body>
+	<div id="container">
+		<div id="image_div">
+			<p style="margin: 0;">
+				<img src="images/dJOkSt.png" class="dJOkSt_img">	
+			</p>
+		</div>
+		<div id="left_sec">
+			<p>A downloadable game for Windows, macOS, and Linux</p>
+			<div id="buy_div1">
+				<a href="https://renderpi.itch.io/the-true-ingredients" target="_blank"><button class="buy_btn">Buy Now</button></a>
+				<h3 style="float: left; margin-top: 5px; margin-left: 5px;">$4.99 USD</h3>
+				<p style="opacity: .5; margin-top: 7px; margin-left: 5px; float: left;">or more</p>
+			</div>
+			<div id="content">
+				<h1>The True Ingredients</h1>
+				<iframe width="500" height="250" class="frame" src="https://www.youtube.com/embed/orXCm-k8c94" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+				<p style="font-weight: bold;">About</p>
+				<p>This game is about a fish living in Underwearzip city. He was hungry so he went to the Krunchy Kans restaurant to eat a Human Patty.  Everything is going to be F̸͓̪͇̝̘̋̇̄̇̓̚Ǐ̵̭͕̪͋͂Ǹ̶̨͉͍͐̆̏͝È̶̗̹̩͖̦̽̔̅͑.</p>
+				<p><b>WARNING:</b> This game includes flashing lights, jump scares, and funny jokes! So if you are allergic to funny jokes please don't play this game!... You can still buy it tho...</p><br>					
+				<p style="font-weight: bold;">Version 1.1</p>
+				<p>All copyrighted content is removed</p>
+				<p><em>Please let us know if there are any other bugs that you guys have found...</em></p>
+				<p style="text-decoration: line-through;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+				<blockquote style="border-left: 4px solid;"><em style="margin-left: 10px;">This game is the n-th indie horror game I made.  let me know what you think about the game in the comments below, <b>your support will really help me to develop my (n+1)th game!</b> and please report any bugs or lags. Thank you.</em></blockquote>
+				<p style="text-decoration: line-through;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+				<h3>FOLLOW US</h3>
+				<p>Website: <a href="https://www.renderpi.com/" target="_blank" style="color: #D9313A;">renderpi.com</a></p>
+				<p>Youtube: <a href="https://www.youtube.com/@renderpigames" target="_blank" style="color: #D9313A;">https://www.youtube.com/@renderpigames</a></p>
+				<p>Discord ID: renderpi#3867</p>
+				<p>Discord Server: <a href="https://discord.gg/hBzMCQ8ar4" target="_blank" style="color: #D9313A;">https://discord.gg/hBzMCQ8ar4</a></p>
+				<p>Email: renderpi@outlook.com</p>
+				<p>Other: <a href="https://linktr.ee/renderpi" target="_blank" style="color: #D9313A;">https://linktr.ee/renderpi</a></p>
+				<p style="text-decoration: line-through;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+			</div>
+			<h2>Purchase</h2>
+			<div id="buy_div2">
+				<a href="https://renderpi.itch.io/the-true-ingredients" target="_blank"><button class="buy_btn">Buy Now</button></a>
+				<h3 style="float: left; margin-top: 5px; margin-left: 5px;">$4.99 USD</h3>
+				<p style="opacity: .5; margin-top: 7px; margin-left: 5px; float: left;">or more</p>
+			</div>
+			<p>In order to download this game you must purchase it at or above the minimum price of $4.99 USD. You will get access to the following files:</p>
+			<div id="windows">
+				<p style="float: left; margin: 0;" title="TheTrueIngredients-WindowsPC-V1.1.zip"><b>TheTrueIngredients-WindowsPC-V1.1.zip</b></p>
+				<p style="float: left; margin: 0; opacity: .5; margin-left: 5px;">163 MB<i style="margin-left: 5px;" class="fa fa-windows"></i></p>
+			</div><br>
+			<div id="apple">
+				<p style="float: left; margin: 0;" title="the-true-ingredients-macos-v1.1.zip"><b>the-true-ingredients-macos-v1.1.zip</b></p>
+				<p style="float: left; margin: 0; opacity: .5; margin-left: 5px;">177 MB<i style="margin-left: 5px;" class="fa fa-apple"></i></p>
+			</div><br>
+			<div id="linux">
+				<p style="float: left; margin: 0;" title="TheTrueIngredients-Linux-V1.1.zip"><b>TheTrueIngredients-Linux-V1.1.zip</b></p>
+				<p style="float: left; margin: 0; opacity: .5; margin-left: 5px;">169 MB<i style="margin-left: 5px;" class="fa fa-linux"></i></p>
+			</div>
+			<h2>Development log</h2>
+			<div id="log1">
+				<a style="color: #D9313A; float: left;" href="https://renderpi.itch.io/the-true-ingredients/devlog/402304/ishowspeed-played-the-true-ingredients" target="_blank">iShowSpeed Played The True Ingredients?</a>
+				<p style="float: left; margin: 0; margin-left: 5px; opacity: .5;">Jul 09, 2022</p>
+			</div>
+			<div id="log2">
+				<a style="color: #D9313A; float: left;" href="https://renderpi.itch.io/the-true-ingredients/devlog/398615/macos-fixed-the-true-ingrenients" target="_blank">macOS fixed: The True Ingrenients</a>
+				<p style="float: left; margin: 0; margin-left: 5px; opacity: .5;">Jun 29, 2022</p>
+			</div>
+			<div id="log3">
+				<a style="color: #D9313A; float: left;" href="https://renderpi.itch.io/the-true-ingredients/devlog/395790/new-release-from-renderpi" target="_blank">New Release from RenderPi</a>
+				<p style="float: left; margin: 0; margin-left: 5px; opacity: .5;">Jun 22, 2022</p>
+			</div>
+			<h2>Comments</h2>
+			<div id="comments_div">
+				<form action="index.php" method="POST">
+					<input type="text" name="content" class="input_text" placeholder="Write your comment...">
+					<input type="submit" class="input_submit" value="Post comment" name="submit">
+				</form>
+				<?php
+					if (isset($_POST['content']) && $_POST['content'] !=""){
+						$content = $_POST['content'];
+						$base = mysqli_connect("localhost", "root", "", "patrick");
+						$que1 = "INSERT INTO data VALUES('1', '$content')";
+						mysqli_query($base, $que1);
+						mysqli_close($base);
+					}
+				?>
+				<br>
+				<div id="comments_div2">
+					<?php
+						$base = mysqli_connect("localhost", "root", "", "patrick");
+						$que2 = "SELECT * FROM data";
+						$result = mysqli_query($base, $que2);
+						while($a = mysqli_fetch_array($result)){
+							echo "<img src='images/frog.png'> `Your Nick`<br>".$a['content']."<br><br>";
+						}
+						mysqli_close($base);
+					?>
+				</div>
+			</div>
+		</div>
+		<div id="right_sec">
+			<p style="margin: 0;">
+				<img src="gif/GOnKHA.gif" width="300" height="200">
+			</p>
+			<p style="margin: 0;">
+				<img src="images/xb8cFe.png" width="300" height="200">
+			</p>
+			<p style="margin: 0;">
+				<img src="images/h93cx1.png" width="300" height="200">
+			</p>
+			<p style="margin: 0;">
+				<img src="images/GC1s0G.png" width="300" height="200">
+			</p>
+			<p style="margin: 0;">
+				<img src="images/A4Th8I.png" width="300" height="200">
+			</p>
+			<p style="margin: 0;">
+				<img src="images/57b+gk.png" width="300" height="200">
+			</p>
+			<p style="margin: 0;">
+				<img src="images/AoZQ3s.png" width="300" height="200">
+			</p>
+		</div>
+	</div>
+</body>
+</html>
